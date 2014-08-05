da836bd4548c0753e570dc275469fec089bf4c8e
Moahmed Aturban <maturban@cs.odu.edu>
2014-08-05 02:12:14 -0400

    add one more run option and modify the existing command line option

--- WARCMerge.py
+++ WARCMerge.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-# Author: Mohamed Aturban, Old Dominion University, CS department, WS-DL Group
+# Author: Mohamed Aturban, WS-DL Group, CS department, Old Dominion University
 import datetime
+import getopt
 import sys 
 import os
   
@@ -65,9 +66,212 @@ def isWarcValid(warcfile):
 		pass
 	return flagV	
 
-		                        ############################################								
+def showUsage():
+	print 'test.py -i <inputfile> -o <outputfile>'
+	sys.exit(0)
+	
 
-# Merging to an existing WARC file
+		                        ############################################								
+#print len(sys.argv)
+# [0] = WARCMerge.py
+#for i in range(len(sys.argv)):
+#	print i
+#	print sys.argv[i]
+#usage: test.py -i <inputfile> -o <outputfile>	
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:],"ha",)
+except getopt.GetoptError:
+	showUsage()
+if len(opts) == 0:
+	if len(args) < 2:
+		showUsage()
+	# merging WARCs into a new WARC file	
+	else: 
+		Ddir = ''
+		dirTree = []
+		# meging WARCs in a directory into an output dirctory
+		if len(args) == 2:
+			Sdir = args[0]#sys.argv[1]
+			Ddir = args[1]#sys.argv[2]		
+			if os.path.isdir(Sdir) == False:
+				print('\n  No such directory: '+Sdir+' \n')
+				print('\n  use -a to append one file to another \n')
+				sys.exit(0)
+			if os.path.isdir(Ddir) == False:
+				# Create the destination file if possible
+				os.makedirs(Ddir)			
+			dirTree = list_files(Sdir)
+			if len(dirTree) == 0:
+				print('\n  No WARCs found in the directory: '+Sdir+' \n')
+				sys.exit(0)
+		# meging list of WARCs into an output dirctory
+		else:
+			Ddir = args[-1]
+			if os.path.isdir(Ddir) == False:
+				# Create the destination file if possible
+				os.makedirs(Ddir)			
+			flagCheckFiles = 0
+			for f in args[:-1]:
+				if (os.path.isfile(f) == True) and (f.endswith('.warc') == True):
+					dirTree.append(f)
+				else:
+					flagCheckFiles = 1
+			if flagCheckFiles == 1:
+				print "\n The given list contains one or more files that are not in valid WARC format"
+			if len(dirTree) == 0:
+				print('\n  No WARC files found in given list \n')
+				sys.exit(0)	
+
+		# Meging files 
+
+		#outputPath = "./merging-WARCs/"
+		#if not os.path.exists(outputPath):
+		#	os.makedirs(outputPath)
+		outputPath	= Ddir
+		# create a new folder 	
+		#outputPath = outputPath + timeStampedFilename("WARCMerge")
+		#os.makedirs(outputPath)
+
+		# generate new warc file name
+		newFile = timeStampedFilename("WARCMerge")+'.warc'
+		newFileFullPath = outputPath+'/'+newFile
+		filePtr = warc.open(newFileFullPath, "w")
+		flag = 0
+
+		outputFileSize = os.path.getsize(newFileFullPath) / forConvertToMB
+
+		# Sorting files by sizes
+		sortFiles(dirTree)
+
+		print 
+		print 'Merging the following WARC files: ' 
+		print '----------------------------------: ' 
+
+		for warcFile in dirTree:
+			outputFileSize = os.path.getsize(newFileFullPath) / forConvertToMB
+			fileSize = os.path.getsize(warcFile) / forConvertToMB
+			if ((fileSize + outputFileSize) > MaxWarcSize) and (outputFileSize != 0) :
+				filePtr.close()
+				# generate new warc file name if current file exceeds WARC file size limit
+				newFile = timeStampedFilename("WARCMerge")+'.warc'
+				newFileFullPath = outputPath+'/'+newFile
+				filePtr = warc.open(newFileFullPath, "w")
+				flag = 0
+		
+			f = warc.WARCFile(warcFile, "rb")
+			try:
+				for record in f:
+					if flag == 0:
+						R = createWarcInfoReacord(newFile)
+						filePtr.write_record(R)
+						flag = 1
+					if ("warcinfo" in record['WARC-Type']):
+						New_Payload = record.payload.read().strip()+ "\r\n" +"WARC-appended-by-WARCMerge: "+datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + "\r\n"
+						record['Content-Length'] = str(len(New_Payload))
+						R = warc.WARCRecord(record.header , New_Payload, defaults=False)
+					else:	
+						R = warc.WARCRecord(payload=record.payload.read(), headers=record.header, defaults=False)
+					filePtr.write_record(R)
+				print '[ Yes ]' + warcFile	
+			except Exception as e:
+				#print("Exceptionq: %s"%(str(e)))
+				print '[ No ]' + warcFile
+				pass
+		filePtr.close()
+		outputFileSize = os.path.getsize(newFileFullPath) / forConvertToMB
+		if outputFileSize == 0:
+			os.remove(newFileFullPath)
+
+		print '\nValidating the resulting WARC files: ' 
+		print '----------------------------------: ' 
+
+		dirTreeCheckWARCs = list_files(outputPath)
+		
+		for filePath in dirTreeCheckWARCs:
+			correct = isWarcValid(filePath)
+			if correct == -2:
+				break
+			if correct == 0:
+				print '- [ valid ]\t'+filePath+''
+			else:
+				if correct == -1:
+					print '- [ Invalid ]\t'+filePath+''
+				else:
+					print '- [ WARC validator (jwattools.sh) cannot be run ]\t\t'+filePath+''
+		print				
+elif len(opts) == 1:
+		if opts[0][0] == '-h':
+			showUsage()
+		elif opts[0][0] == '-a':
+			################ Appending ############### 
+			# Appending to an existing WARC file
+			#if len(sys.argv) == 3:
+			Sfile = args[0]#sys.argv[1]
+			Dfile = args[1]#sys.argv[2]
+			if not os.path.isfile(Sfile):
+				print('\n  No such file: '+Sfile+' \n')
+				sys.exit(0)	
+			if not os.path.isfile(Dfile):
+				print('\n  No such file: '+Dfile+' \n')
+				sys.exit(0)
+			if (not Sfile.endswith(".warc")):
+				print ("\n Source file ("+Sfile+") is not WARC file")
+				sys.exit(0)
+			if (not Dfile.endswith(".warc")):
+				print ("\n Dest file ("+Dfile+") is not WARC file")
+				sys.exit(0)
+			correct = isWarcValid(Sfile)
+			if correct == -2:
+				print ("Warning: WARC validator (jwattools.sh) cannot be run")
+			else:
+				if correct != 0:
+					print ("\n Source file ("+Sfile+") is not valid WARC file")
+					sys.exit(0)		
+			correct = isWarcValid(Dfile)
+			if correct == -2:
+				print ("Warning: WARC validator (jwattools.sh) cannot be run")
+			else:
+				if correct != 0:
+					print ("\n Dest file ("+Dfile+") is not valid WARC file")
+					sys.exit(0)
+			if (( (os.path.getsize(Sfile)/ forConvertToMB) + (os.path.getsize(Dfile)/ forConvertToMB)) > MaxWarcSize):
+				print ("\n Connot merge <Souce> file with <Dest> file: [Maximum <Dest> WARC file is "+str(MaxWarcSize)+" MB].")
+				sys.exit(0)
+			try:
+				filePtr = warc.open(Dfile, "a")
+			except Exception as e:
+				print 'Error in writing in:' + Dfile
+				sys.exit(0)
+			try:
+				f = warc.WARCFile(Sfile, "rb")
+				for record in f:
+					if ("warcinfo" in record['WARC-Type']):
+						New_Payload = record.payload.read().strip()+ "\r\n" +"WARC-appended-by-WARCMerge: "+datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ') + "\r\n"
+						record['Content-Length'] = str(len(New_Payload))
+						R = warc.WARCRecord(record.header , New_Payload, defaults=False)
+					else:	
+						R = warc.WARCRecord(payload=record.payload.read(), headers=record.header, defaults=False)
+					filePtr.write_record(R)
+			except Exception as e:
+				print 'Error in reading:' + Sfile
+				sys.exit(0)
+			f.close()		
+			filePtr.close()
+			correct = isWarcValid(Dfile)
+			if correct ==0:
+				print ("\n\t The resulting Dest: ("+Dfile+") is valid WARC file")
+			if correct != 0:
+				print ("\n The resulting Dest file ("+Dfile+") is not valid WARC file")
+				sys.exit(0)
+else:
+	showUsage()		
+
+
+
+sys.exit(0)
+
+# Appending to an existing WARC file
 if len(sys.argv) == 3:
 	Sfile = sys.argv[1]
 	Dfile = sys.argv[2]
